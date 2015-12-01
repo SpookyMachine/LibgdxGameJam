6e6298b2a78bd4d22f351cb7a2d6679db503fecd
Mike <insert1789@gmail.com>
2015-12-01 21:45:09 +0200

    init project setup

---
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.game.LibgdxGameJamTestGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LibgdxGameJamTestGame();
+        }
+}
