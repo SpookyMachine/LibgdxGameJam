9690bf9c174d50ba8d9e49d08be445a503275c86
gadge <programuotojasgytis@outlook.com>
2015-12-12 16:35:58 +0200

    TASK-03 2. created simple falling mechanic

---
+++ core/src/com/mygdx/game/actor/AbstractPart.java
@@ -0,0 +1,19 @@
+package com.mygdx.game.actor;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by Gt
+ */
+public class AbstractPart extends Actor {
+
+    protected static final float SPEED_SLOW = 7f;
+
+    protected static final float SPEED_NORMAL = 5f;
+
+    protected static final float SPEED_FAST = 3f;
+
+    protected static final float SPEED_LIGHTNING_FAST = 1f;
+
+
+}
--- core/src/com/mygdx/game/actor/TreePart.java
+++ core/src/com/mygdx/game/actor/TreePart.java
@@ -4,22 +4,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
+
 
 /**
  * Created by Gt
  */
-public class TreePart extends Actor {
+public class TreePart extends AbstractPart {
 
     private ShapeRenderer renderer = new ShapeRenderer();
 
     private static final Color COLOR_DARK_GREEN = new Color(0, 0.4f, 0, 1);
 
+    public TreePart() {
+//        setBounds(); TODO:
+
+        MoveByAction mba = new MoveByAction();
+        mba.setAmountY(Gdx.graphics.getHeight());
+        mba.setDuration(SPEED_SLOW);
+        mba.setReverse(true); // from top, to bot
+
+        this.addAction(mba);
+    }
+
     @Override
     public void draw(Batch batch, float parentAlpha) {
         batch.end();
 
-        drawArea(0); // TODO: pass random
+        drawArea(); // TODO: pass random x
 
         batch.begin();
     }
@@ -27,14 +39,16 @@ public class TreePart extends Actor {
     @Override
     public void act(float delta) {
         super.act(delta);
+
+
     }
 
     /**
      * Draw the block @ x. y = max height (Need to end batch before this method and begin after).
      */
-    private void drawArea(float x) {
+    private void drawArea() {
         renderer.begin(ShapeRenderer.ShapeType.Filled);
-        renderer.rect(x, 0,
+        renderer.rect(getX(), getY(),
                 40f, 40f,
                 COLOR_DARK_GREEN, Color.GREEN, Color.GREEN, COLOR_DARK_GREEN);
         renderer.end();
