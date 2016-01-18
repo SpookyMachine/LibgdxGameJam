769d0f37078acd0cf80f189e2f22f9e7fe863072
Mike <insert1789@gmail.com>
2016-01-18 23:03:02 +0200

    fisniehd with chapter 2

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -6,13 +6,17 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by mike on 1/18/16.
  */
 public class GameScreen extends ScreenAdapter {
 
+    // Sammy stuff
     private static final float MOVE_TIME = 1f;
     private static final int SNAKE_MOVEMENT = 32;
 
@@ -21,58 +25,94 @@ public class GameScreen extends ScreenAdapter {
     private static final int UP = 2;
     private static final int DOWN = 3;
 
-
-
     private float timer = MOVE_TIME;
     private int snakeX = 0, snakeY = 0;
     private int snakeDirection = RIGHT;
 
-    private SpriteBatch batch;
-    private Texture snakeHead;
+    private int snakeXBeforeUpdate = 0, snakeYBeforeUpdate = 0;
 
+    // Apple stuff
+    private boolean appleAvailable = false;
+    private int appleX, appleY;
+
+
+    // Random shit
+    private SpriteBatch batch;
+
+    private Texture snakeHead;
+    private Texture apple;
+    private Texture snakeBody;
+
+    private Array<BodyPart> bodyParts = new Array<BodyPart>();
 
 
     @Override
     public void show() {
         batch = new SpriteBatch();
         snakeHead = new Texture(Gdx.files.internal("snakehead.png"));
+        apple = new Texture(Gdx.files.internal("apple.png"));
+        snakeBody = new Texture(Gdx.files.internal("snakeBody.png"));
     }
 
     @Override
     public void render(float delta) {
         queryInput();
-        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         timer -= delta;
-        if(timer <=0 ){
+        if (timer <= 0) {
             timer = MOVE_TIME;
-        moveSnake();
+            moveSnake();
+            checkForOutOfBounds();
+            updateBodyPartsPosition();
         }
-
-        checkForOutOfBounds();
-        batch.begin();
-        batch.draw(snakeHead, snakeX, snakeY );
-        batch.end();
+        checkAppleCollision();
+        checkAndPlaceApple();
+        clearScreen();
+        draw();
     }
 
-    private void checkForOutOfBounds(){
+    private void clearScreen() {
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    public void draw(){
+        batch.begin();
+
+        batch.draw(snakeHead, snakeX, snakeY);
+        for (BodyPart bodyPart : bodyParts){
+            bodyPart.draw(batch);
+        }
+        if (appleAvailable) {
+            batch.draw(apple, appleX, appleY);
+        }
+
+        batch.end();
+
+    }
+
+
+    private void checkForOutOfBounds() {
         if (snakeX >= Gdx.graphics.getWidth()) {
-            snakeX = 0; }
+            snakeX = 0;
+        }
         if (snakeX < 0) {
             snakeX = Gdx.graphics.getWidth() - SNAKE_MOVEMENT;
         }
         if (snakeY >= Gdx.graphics.getHeight()) {
-            snakeY = 0; }
+            snakeY = 0;
+        }
         if (snakeY < 0) {
             snakeY = Gdx.graphics.getHeight() - SNAKE_MOVEMENT;
         }
     }
 
-    private void moveSnake(){
-        switch (snakeDirection){
+    private void moveSnake() {
+        snakeXBeforeUpdate = snakeX;
+        snakeYBeforeUpdate = snakeY;
+        switch (snakeDirection) {
             case RIGHT: {
                 snakeX += SNAKE_MOVEMENT;
+
                 return;
             }
             case LEFT: {
@@ -90,7 +130,7 @@ public class GameScreen extends ScreenAdapter {
         }
     }
 
-    private void queryInput(){
+    private void queryInput() {
         boolean lPressed = Gdx.input.isKeyPressed(Input.Keys.LEFT);
         boolean rPressed = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
         boolean uPressed = Gdx.input.isKeyPressed(Input.Keys.UP);
@@ -99,6 +139,57 @@ public class GameScreen extends ScreenAdapter {
         if (rPressed) snakeDirection = RIGHT;
         if (uPressed) snakeDirection = UP;
         if (dPressed) snakeDirection = DOWN;
+    }
+
+    private void checkAndPlaceApple() {
+        if (!appleAvailable) {
+            do {
+                appleX = MathUtils.random(Gdx.graphics.getWidth()
+                        / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+                appleY = MathUtils.random(Gdx.graphics.getHeight()
+                        / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+                appleAvailable = true;
+            } while (appleX == snakeX && appleY == snakeY);
+
+        }
+    }
+
+    public void checkAppleCollision(){
+        if(appleAvailable && appleX == snakeX && appleY == snakeY){
+            BodyPart bodyPart = new BodyPart(snakeBody);
+            bodyPart.updateBodyPosition(snakeX, snakeY);
+            bodyParts.insert(0, bodyPart);
+            appleAvailable = false;
+        }
+    }
+
+    private void updateBodyPartsPosition(){
+        if(bodyParts.size > 0){
+            BodyPart bodyPart = bodyParts.removeIndex(0);
+            bodyPart.updateBodyPosition(snakeXBeforeUpdate, snakeYBeforeUpdate);
+            bodyParts.add(bodyPart);
+        }
+    }
+
+
+    public class BodyPart {
+
+        private int x, y;
+        private Texture texture;
+
+        public BodyPart(Texture texture) {
+            this.texture = texture;
+        }
+
+        public void updateBodyPosition(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public void draw(Batch batch){
+            if(!(x == snakeX && y == snakeY))
+                batch.draw(texture, x,y);
+        }
     }
 
 }
