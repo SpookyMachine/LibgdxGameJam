617a74d18b48eb9f6d49c0c91b6e42f3aff63f19
Mike <insert1789@gmail.com>
2016-01-20 21:32:19 +0200

    inited default setup for flappy bird

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,75 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+/**
+ * Created by mike on 1/20/16.
+ */
+public class GameScreen extends ScreenAdapter {
+
+    private static final float WORLD_WIDTH = 480;
+    private static final float WORLD_HEIGHT = 640;
+
+    private ShapeRenderer shapeRenderer;
+    private Viewport viewport;
+    private Camera camera;
+    private SpriteBatch batch;
+
+
+    @Override
+    public void render(float delta) {
+        clearScreen();
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g,
+                Color.BLACK.b, Color.BLACK.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void show() {
+
+        camera = new OrthographicCamera();
+        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
+        camera.update();
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        shapeRenderer = new ShapeRenderer();
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
